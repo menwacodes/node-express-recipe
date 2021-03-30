ff054c880e5d731992aa29ecb616f5e8e9bd9b09
menwa <menwa.codes@gmail.com>
2021-03-30 16:15:11 -0400

    Added login, logout, register to nav bar

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 
 const User = require('../models/user');
+const passport = require("passport");
 
 // show registration form
 router.get('/register', (req, res) => {
@@ -11,18 +12,44 @@ router.get('/register', (req, res) => {
 });
 
 // create user
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
     try {
         const {username, password, email, name} = req.body;
         const newUser = await User({username, email, name});
         const registeredUser = await User.register(newUser, password);
-        req.flash('success', 'Successfully Registered')
-        res.redirect('/recipes')
+
+        req.login(registeredUser, err => {if (err) return next(err)})
+
+        req.flash('success', 'Successfully Registered');
+        res.redirect('/recipes');
 
     } catch (err) {
-        req.flash('error', err.message)
+        req.flash('error', err.message);
         res.redirect('/recipes/welcome/register');
     }
 });
+
+// login user form
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+// log the user in
+router.post('/login',
+    passport.authenticate(
+        'local',
+        {failureFlash: true, failureRedirect: '/recipes/welcome/login'}),
+    (req, res) => {
+        // user authed if here
+        req.flash('success', 'Welcome Back');
+        res.redirect('/recipes');
+    });
+
+// log the user out
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', 'Succesfully Logged Out, come back soon')
+    res.redirect('/recipes')
+})
 
 module.exports = router;
