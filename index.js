99a1acc95ce86d566b7ce807e89772181e1d47f3
menwa <menwa.codes@gmail.com>
2021-03-31 16:30:09 -0400

    ready to try deploying

--- index.js
+++ index.js
@@ -3,19 +3,40 @@ const chalk = require('chalk');
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const mongoSanitize = require('express-mongo-sanitize')
-const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
+let mongoUri = process.env.MONGO_ATLAS_URI;
+const mURL = 'mongodb://localhost:27017/';
+const mDB = 'recipe';
+mongoUri = mURL + mDB;
 
 const flash = require('connect-flash');
 
 
 // auth
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
 const User = require('./models/user');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+
+
+const store = new MongoStore({
+    mongoUrl: mongoUri,
+    crypto: {
+        secret: process.env.APP_SECRET,
+    },
+    touchAfter: 24 * 3600, // update store every 24h only if the page hasn't been changed
+});
+
+store.on('error', function (e) {
+    console.log(e);
+});
+
 const sessionConfig = {
+    store,
     name: "hbgc_29", // changes default name of session
     secret: process.env.APP_SECRET,
     resave: false,
@@ -26,7 +47,7 @@ const sessionConfig = {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
-}
+};
 
 /* START: Express set up */
 const express = require('express');
@@ -45,6 +66,8 @@ app.use(mongoSanitize());
 
 // passport session config
 app.use(session(sessionConfig));
+
+
 app.use(flash());
 
 const scriptSrcUrls = [
@@ -58,8 +81,7 @@ const styleSrcUrls = [
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
 ];
-const connectSrcUrls = [
-];
+const connectSrcUrls = [];
 const fontSrcUrls = [];
 app.use(
     helmet.contentSecurityPolicy({
@@ -86,8 +108,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
-
-
 // assets
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -97,11 +117,11 @@ app.use(methodOverride('_method'));
 // Middleware for all calls
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success')
-    res.locals.error = req.flash('error')
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
     // console.log(res.locals)
-    next()
-})
+    next();
+});
 
 // routes
 const recipeRoutes = require('./routes/recipes');
@@ -112,22 +132,26 @@ const userRoutes = require('./routes/users');
 app.use('/recipes', recipeRoutes);
 app.use('/recipes/:recipeId/ingredients', ingredientRoutes);
 app.use('/search', searchRoutes);
-app.use('/recipes/welcome', userRoutes)
+app.use('/recipes/welcome', userRoutes);
 
-/* mongoose local set up */
-const mURL = 'mongodb://localhost:27017/';
-const mDB = 'recipe';
+/* mongoose local set up
+    comment out and use variable from top if using cloud
+* */
+
+
 const mOptions = {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true};
 
 const connect = async () => {
     try {
-        await mongoose.connect(mURL + mDB, mOptions);
+        await mongoose.connect(mongoUri, mOptions);
         console.log(chalk.bgGreen("Mongo Connection Open"));
     } catch (e) {
         console.log(chalk.bgRed.yellowBright(`Mongo Connection Error: ${e.message}`));
         console.log(e);
     }
 };
+
+// mongodb+srv://admin-menwa:<password>@recipecluster.9nncm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 connect();
 /* END: Mongoose set up */
